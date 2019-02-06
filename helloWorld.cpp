9885e525640b992e8e988991a5054063d7c99bbf
oramirez1397 <omarramirez1397@gmail.com>
2019-02-06 00:29:07 -0800

    added new variable

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -6,5 +6,9 @@ int main()
   cout << "Hello WORLD!!! " << '\n';
   cout << "How are you doing? " << '\n';
   cout << "Hope everyone is well " << '\n';
+  int two = 2;
+
+  cout << two << '\n';
+  
   return 0;
 }
